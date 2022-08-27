448dd0ad4f68de0d98e89bd567608a57345c5db9
Brayan Bravo Arebalo <brayan.bravo.arebalo@gmail.com>
2022-08-27 17:57:08 -0400

    se creo el schema para los usuarios

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    index: {
+      unique: true,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+
+export const User = mongoose.model("user", userSchema)
